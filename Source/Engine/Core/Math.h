9ca64b63cd446acad04bb4829b693b71afd9d908
CodyAndGity <mdavis@student.neumont.edu>
2025-07-01 10:17:01 -0600

    added core files to imprve physical structure

---
+++ Source/Engine/Core/Math.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace bonzai {
+	constexpr float pi = 3.14159265358979323846f;
+	constexpr float twoPi = 2.0f * pi;
+	constexpr float halfPi = 0.5f * pi;
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,4 +1,11 @@
+#include "Core/Math.h"
 #include <iostream>
+
+#define NAME "Raymond"
+
 int main() {
+
+	std::cout << NAME;
 	std::cout << "Hello, World!\n";
+	std::cout << bonzai::pi << std::endl;
 }
